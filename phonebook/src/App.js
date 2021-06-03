004991f6902254030e9c6741e735e4db0c95b1be
Kev Comedia <kevcomedia@gmail.com>
2021-06-03 18:03:39 +0800

    Handle name input change

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -4,12 +4,16 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
   const [newName, setNewName] = useState('')
 
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>

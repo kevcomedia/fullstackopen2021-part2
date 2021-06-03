4cb4334492ef417788fc636c5961a3d813ffed74
Kev Comedia <kevcomedia@gmail.com>
2021-06-03 17:08:20 +0800

    Allow arbitrary number of courses

--- course-information/src/App.js
+++ course-information/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Header = (props) => {
-  return <h1>{props.course}</h1>
+  return <h2>{props.course}</h2>
 }
 
 const Part = (props) => {
@@ -42,33 +42,59 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: 'Rendering a collection',
-        exercises: 5,
-        id: 4,
-      },
-    ],
-  }
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Rendering a collection',
+          exercises: 5,
+          id: 4,
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Node.js',
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ]
 
-  return <Course course={course} />
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      {courses.map((course) => (
+        <Course course={course} />
+      ))}
+    </div>
+  )
 }
 
 export default App

a55e34279dc53f237fbedf5d29748b8af64f7f53
Kev Comedia <kevcomedia@gmail.com>
2021-06-03 17:11:54 +0800

    Refactor Course to its own module

--- course-information/src/App.js
+++ course-information/src/App.js
@@ -1,45 +1,5 @@
 import React from 'react'
-
-const Header = (props) => {
-  return <h2>{props.course}</h2>
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map((part) => (
-        <Part part={part} key={part.id} />
-      ))}
-    </>
-  )
-}
-
-const Total = ({ parts }) => {
-  const total = parts.map((part) => part.exercises).reduce((a, b) => a + b, 0)
-  return (
-    <p>
-      <b>total of {total} exercises</b>
-    </p>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [

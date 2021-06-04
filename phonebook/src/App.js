849ee01446abe81ccf0fbaf5625e7a5dc9616814
Kev Comedia <kevcomedia@gmail.com>
2021-06-04 18:06:01 +0800

    Fetch persons from db.json

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Search from './components/Search'
 import Form from './components/Form'
 import PhonebookEntries from './components/PhonebookEntries'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '1234567' },
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((response) => {
+      setPersons(response.data)
+    })
+  }, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)

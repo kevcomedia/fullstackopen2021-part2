8ca70e9564fefe02fbf49983c6df30822481d2a4
Kev Comedia <kevcomedia@gmail.com>
2021-06-16 18:02:39 +0800

    Use different styles for failure notifications

--- phonebook/src/components/Notification.js
+++ phonebook/src/components/Notification.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Notification = ({ notification }) => {
   if (!notification) return null
 
-  return <div className="notification">{notification.message}</div>
+  const className = `notification ${
+    notification.success ? 'notification__success' : 'notification__failure'
+  }`
+  return <div className={className}>{notification.message}</div>
 }
 
 export default Notification

f063761632f6954cf3a2c30cf2980c617ce617e1
Kev Comedia <kevcomedia@gmail.com>
2022-02-20 20:08:10 +0800

    Reduce delete button font size

--- phonebook/src/components/Button.js
+++ phonebook/src/components/Button.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button = ({ x_type, ...props }) => {
+const Button = ({ x_size, x_type, ...props }) => {
   const colors = {
     danger: 'bg-red-500 hover:bg-red-600 focus:bg-red-600',
     primary: 'bg-blue-500 hover:bg-blue-600 focus:bg-blue-600',
@@ -9,7 +9,9 @@ const Button = ({ x_type, ...props }) => {
   return (
     <button
       {...props}
-      className={`rounded px-3 py-1 ${colors[x_type]} focus:outline-none focus:ring text-white`}
+      className={`rounded px-3 py-1 ${colors[x_type]} ${
+        x_size === 'small' ? 'text-sm' : ''
+      } focus:outline-none focus:ring text-white`}
     />
   )
 }
--- phonebook/src/components/PhonebookEntry.js
+++ phonebook/src/components/PhonebookEntry.js
@@ -9,7 +9,7 @@ const PhonebookEntry = ({ entry, onDelete }) => {
   return (
     <li className="mb-0.5">
       <b className="text-slate-800">{entry.name}</b> <span>{entry.number}</span>{' '}
-      <Button onClick={handleDelete} x_type="danger">
+      <Button onClick={handleDelete} x_type="danger" x_size="small">
         delete
       </Button>
     </li>

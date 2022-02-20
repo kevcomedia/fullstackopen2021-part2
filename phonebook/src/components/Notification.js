91ae885aca1494077a331d53070550a4bb1a3f38
Kev Comedia <kevcomedia@gmail.com>
2022-02-20 20:03:58 +0800

    Replace notification styles with tailwind

--- phonebook/src/components/Notification.js
+++ phonebook/src/components/Notification.js
@@ -3,8 +3,10 @@ import React from 'react'
 const Notification = ({ notification }) => {
   if (!notification) return null
 
-  const className = `notification ${
-    notification.success ? 'notification__success' : 'notification__failure'
+  const className = `border-2 rounded-md px-4 py-3 mb-3 ${
+    notification.success
+      ? 'border-green-700 text-green-700 bg-green-50'
+      : 'border-red-700 text-red-700 bg-red-50'
   }`
   return <div className={className}>{notification.message}</div>
 }
